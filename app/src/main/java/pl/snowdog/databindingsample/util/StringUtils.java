93d6aa389c825ca973f48dbc8bcc25b039a96be8
Bartek Kwiatkowski <bartholomaeus.kwiatkowski@gmail.com>
2015-06-15 12:18:55 +0200

    StringUtils example added

---
+++ app/src/main/java/pl/snowdog/databindingsample/util/StringUtils.java
@@ -0,0 +1,11 @@
+package pl.snowdog.databindingsample.util;
+
+/**
+ * Created by bartek on 15.06.15.
+ */
+public class StringUtils {
+
+    public static String capitalize(String s) {
+        return s.toUpperCase();
+    }
+}
